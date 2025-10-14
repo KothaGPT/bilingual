b750da3f1131c62097f6677b65d021d8bb1933d9
NeoPilot <221231603+neopilotai@users.noreply.github.com>
2025-10-15 00:17:21 +0600

    Fix setuptools deprecation warnings and update build configuration

--- src/bilingual/_version.py
+++ src/bilingual/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.1.1.dev2+g5970b4b0f.d20251014'
-__version_tuple__ = version_tuple = (1, 1, 1, 'dev2', 'g5970b4b0f.d20251014')
+__version__ = version = '1.1.1.dev3+g575945f1f.d20251014'
+__version_tuple__ = version_tuple = (1, 1, 1, 'dev3', 'g575945f1f.d20251014')
 
-__commit_id__ = commit_id = 'g5970b4b0f'
+__commit_id__ = commit_id = 'g575945f1f'
