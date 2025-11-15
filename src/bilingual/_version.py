0dffdc481db1ccd77da2f424ffbe51b1e3175080
envrs <dr.lizadmf@gmail.com>
2025-11-15 16:26:08 +0000

    chore(version): bump version to 1.1.1.dev79

--- src/bilingual/_version.py
+++ src/bilingual/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.1.1.dev23+g392e7f616.d20251115'
-__version_tuple__ = version_tuple = (1, 1, 1, 'dev23', 'g392e7f616.d20251115')
+__version__ = version = '1.1.1.dev79+g635f2deeb.d20251115'
+__version_tuple__ = version_tuple = (1, 1, 1, 'dev79', 'g635f2deeb.d20251115')
 
-__commit_id__ = commit_id = 'g392e7f616'
+__commit_id__ = commit_id = 'g635f2deeb'
