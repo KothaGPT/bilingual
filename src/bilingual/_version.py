44ee1ecc4237a77a7e8f575dae579b6504c31bcd
envrs <dr.lizadmf@gmail.com>
2025-11-15 15:09:32 +0000

    update

--- src/bilingual/_version.py
+++ src/bilingual/_version.py
@@ -12,8 +12,7 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple
-    from typing import Union
+    from typing import Tuple, Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -28,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.1.1.dev15+g19c83a132.d20251115'
-__version_tuple__ = version_tuple = (1, 1, 1, 'dev15', 'g19c83a132.d20251115')
+__version__ = version = "1.1.1.dev15+g19c83a132.d20251115"
+__version_tuple__ = version_tuple = (1, 1, 1, "dev15", "g19c83a132.d20251115")
 
-__commit_id__ = commit_id = 'g19c83a132'
+__commit_id__ = commit_id = "g19c83a132"
